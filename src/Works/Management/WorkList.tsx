30e92d3848f7163ce5d99ef1c95b2d5c3dff4dbe
Tiki Torch <atomic.tikitorch@gmail.com>
2023-03-27 15:10:41 +0900

    add #3 作品を作成

--- src/Works/Management/WorkList.tsx
+++ src/Works/Management/WorkList.tsx
@@ -2,12 +2,12 @@ import  { GenerativeWork } from './GenerativeWork'
 import { SampleWork1 } from './Samples/SampleWork1'
 import { SampleWork2 } from './Samples/SampleWork2'
 import { NotFound } from './NotFound'
-import { WorkA } from '../Workfiles/WorkA'
+import { JustLowerRightRotateFaster } from '../Workfiles/JustLowerRightRotateFaster'
 
 
 export const WORK_LIST: Array<any> = [
   SampleWork1,
   SampleWork2,
-  WorkA,
+  JustLowerRightRotateFaster,
   NotFound
 ]
