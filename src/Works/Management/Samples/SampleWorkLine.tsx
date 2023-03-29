5c7952644ad72892e9e8864f8eb8f8fa01699027
Tiki Torch <atomic.tikitorch@gmail.com>
2023-03-29 14:42:39 +0900

    add #7 lineを用いたサンプル作品を作成

--- src/Works/Management/Samples/SampleWorkLine.tsx
+++ src/Works/Management/Samples/SampleWorkLine.tsx
@@ -18,7 +18,9 @@ export class SampleWorkLine extends GenerativeWork{
     super();
 
     // 新しい3Dモデルを生成
-    const generated = this.generateLine();
+    const time      = Date.now() / 1000;
+    const points    = this.generatePoints(time);
+    const generated = this.generateLine(points);
     this.tdobjs = [ generated ];
     
   }
@@ -27,19 +29,41 @@ export class SampleWorkLine extends GenerativeWork{
 
     // アニメーション
     this.tdobjs.forEach( (line) => {
-      line.rotation.y += 0.01;
+
+      const time      = Date.now() / 1000;
+      const newPoints = this.generatePoints(time);
+      line.geometry.setFromPoints(newPoints)
+
+      // 描画を更新
       line.geometry.attributes.position.needsUpdate = true;
     })
 
   };
 
-  generateLine(): THREE.Line{
+  generatePoints(time: number): Array<THREE.Vector3>{
+
+    const AMPLITUDE   = 100;
+    const PERIOD      = 180;
+    const LENGTH      = 720;
+    const START_POINT = { x: -500, y: 0, z: 0 };
+
     const points = [];
-    points.push(new THREE.Vector3(-100, 0, 0));
-    points.push(new THREE.Vector3(0, 100, 0));
-    points.push(new THREE.Vector3(100, 0, 0));
+    for(let i = 0; i < LENGTH ; i++){
+      const radian = (i / PERIOD) * Math.PI + time;
+      points.push(
+        new THREE.Vector3(
+          START_POINT.x + i,
+          START_POINT.y + Math.sin(radian) * AMPLITUDE,
+          START_POINT.z
+        )
+      );
+    }
+    return points
+  }
+
+  generateLine(points: Array<THREE.Vector3>): THREE.Line{
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    const material = new THREE.LineBasicMaterial({color: 0xffffff});;
+    const material = new THREE.LineBasicMaterial({ color: 0xffffff });;
     const line = new THREE.Line(geometry, material);
     return line
   }
